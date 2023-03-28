f3c9803ccf7109b06da31c2d4ff01501ed4dccd7
Benjamin VIELLARD <bicou@bicou.com>
2023-03-28 22:09:29 +0200

    chore: use sass for field component

--- src/interface.vue
+++ src/interface.vue
@@ -1,16 +1,55 @@
 <template>
-  <editor-content class="tiptap-editor" :editor="editor" />
+    <div class="tiptap-editor">
+        <!--
+        <button @click="editor.chain().focus().toggleBold().run()">
+            Bold
+        </button>
+        -->
+        <editor-content class="tiptap-editor__content" :editor="editor" />
+    </div>
 </template>
 
-<style lang="css">
+<style lang="scss">
 .tiptap-editor {
-  padding: var(--input-padding);
   color: var(--v-input-color);
   font-family: var(--v-input-font-family);
   background-color: var(--v-input-background-color);
   border: var(--border-width) solid var(--border-normal);
   border-radius: var(--border-radius);
   transition: border-color var(--fast) var(--transition);
+
+  .tiptap-editor__content {
+    padding: var(--input-padding);
+  }
+
+  &:hover {
+    --arrow-color: var(--border-normal-alt);
+    color: var(--v-input-color);
+    background-color: var(--background-input);
+    border-color: var(--border-normal-alt);
+  }
+
+  &:focus-within,
+  &.active {
+    --arrow-color: var(--border-normal-alt);
+    color: var(--v-input-color);
+    background-color: var(--background-input);
+    border-color: var(--v-input-border-color-focus);
+    box-shadow: 0 0 16px -8px var(--v-input-box-shadow-color-focus);
+  }
+
+  &.disabled {
+    --arrow-color: var(--border-normal);
+    color: var(--foreground-subdued);
+    background-color: var(--background-subdued);
+    border-color: var(--border-normal);
+  }
+
+  .ProseMirror {
+    > * + * {
+      margin-top: 0.75em;
+    }
+  }
 }
 </style>
 
