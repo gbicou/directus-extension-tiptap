a64b2088164a17337c3ac0c9a0a5c096e75651b3
Benjamin VIELLARD <bicou@bicou.com>
2023-03-30 21:13:44 +0200

    fix: run heading command

--- src/tiptap-editor.vue
+++ src/tiptap-editor.vue
@@ -81,7 +81,7 @@
             :key="n"
             clickable
             :active="editor.isActive('heading', { level: n })"
-            @click="editor.chain().focus().toggleHeading({ level: n })"
+            @click="editor.chain().focus().toggleHeading({ level: n }).run()"
           >
             <v-list-item-content><v-text-overflow :text="t(`wysiwyg_options.h${n}`)" /></v-list-item-content>
             <v-list-item-hint>{{ translateShortcut(["meta", "alt"]) }} {{ n }}</v-list-item-hint>
