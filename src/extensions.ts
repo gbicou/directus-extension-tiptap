d8b1748ff724967343d608f120f9e4b5a3ff8200
Benjamin VIELLARD <gbicou@gmail.com>
2023-04-06 15:18:13 +0200

    feat: highlight extension

--- src/extensions.ts
+++ src/extensions.ts
@@ -1,11 +1,15 @@
 import StarterKit from "@tiptap/starter-kit";
 import type { Extensions } from "@tiptap/core";
 
-export async function loadExtensions(underline = true): Promise<Extensions> {
+export async function loadExtensions(underline = true, highlight = true): Promise<Extensions> {
   const extensions: Extensions = [StarterKit];
   if (underline) {
     const { Underline } = await import("@tiptap/extension-underline");
     extensions.push(Underline);
+  }
+  if (highlight) {
+    const { Highlight } = await import("@tiptap/extension-highlight");
+    extensions.push(Highlight);
   }
   return extensions;
 }
