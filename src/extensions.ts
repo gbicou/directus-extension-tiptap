b2f2634ff5d392a2b62b0e936a5a1dc491193ecc
Benjamin VIELLARD <gbicou@gmail.com>
2023-04-06 12:39:04 +0200

    feat: make underline extension optional #3

--- src/extensions.ts
+++ src/extensions.ts
@@ -1,12 +1,7 @@
 import StarterKit from "@tiptap/starter-kit";
 import type { Extensions } from "@tiptap/core";
 
-// export const extensions: Extensions = [StarterKit];
-
 export async function loadExtensions(underline = true): Promise<Extensions> {
-  const test = await new Promise<string>((resolve) => setTimeout(() => resolve("OK 1sec"), 1000));
-  console.debug(test);
-
   const extensions: Extensions = [StarterKit];
   if (underline) {
     const { Underline } = await import("@tiptap/extension-underline");
