5e6931cf47325dd747eb4930673389c5ab791223
Benjamin VIELLARD <gbicou@gmail.com>
2023-11-14 10:26:34 +0100

    docs: docstrings

--- src/composables/image.ts
+++ src/composables/image.ts
@@ -2,10 +2,26 @@ import type { Editor } from "@tiptap/vue-3";
 import { ref } from "vue";
 import type { ImageAttributes } from "../extensions/image";
 
+/**
+ * Composable for managing image functionality in the tiptap editor.
+ *
+ * @param editor The tiptap editor instance.
+ * @returns An object containing image functions and state.
+ */
 export function useImage(editor: Editor) {
+  /**
+   * Open state of image drawer.
+   */
   const imageDrawerOpen = ref(false);
+
+  /**
+   * Image selection.
+   */
   const imageSelection = ref<ImageAttributes | null>(null);
 
+  /**
+   * Opens the image selector and sets the value of the image selection.
+   */
   function imageOpen() {
     if (editor.isActive("image")) {
       imageSelection.value = editor.getAttributes("image") as ImageAttributes;
@@ -15,11 +31,17 @@ export function useImage(editor: Editor) {
     imageDrawerOpen.value = true;
   }
 
+  /**
+   * Closes the image drawer and clears the image selection.
+   */
   function imageClose() {
     imageDrawerOpen.value = false;
     imageSelection.value = null;
   }
 
+  /**
+   * Sets the selected image in the editor.
+   */
   function imageSave() {
     if (imageSelection.value) {
       editor.chain().focus().setImage(imageSelection.value).run();
@@ -27,6 +49,11 @@ export function useImage(editor: Editor) {
     imageClose();
   }
 
+  /**
+   * Selects a directus image and updates the image selection value.
+   *
+   * @param image The directus image object to be selected.
+   */
   function imageSelect(image: Record<string, never>) {
     imageSelection.value = {
       id: image.id,
--- src/composables/link.ts
+++ src/composables/link.ts
@@ -1,11 +1,29 @@
 import type { Editor } from "@tiptap/vue-3";
 import { ref } from "vue";
 
+/**
+ * Composable providing functionality for managing links in the tiptap editor
+ *
+ * @param {Editor} editor - The Tiptap editor instance
+ * @returns An object containing link functions and state.
+ */
 export function useLink(editor: Editor) {
+  /**
+   * ref tracking if the link drawer UI is open.
+   */
   const linkDrawerOpen = ref(false);
+  /**
+   * ref tracking the href for the link being edited.
+   */
   const linkHref = ref("");
+  /**
+   * ref tracking the target for the link being edited.
+   */
   const linkTarget = ref("");
 
+  /**
+   * Opens the link drawer and populates href/target refs from current link.
+   */
   function linkOpen() {
     const attrs = editor.getAttributes("link");
     linkHref.value = attrs.href;
@@ -13,12 +31,18 @@ export function useLink(editor: Editor) {
     linkDrawerOpen.value = true;
   }
 
+  /**
+   * Closes the link drawer and resets href/target refs.
+   */
   function linkClose() {
     linkDrawerOpen.value = false;
     linkHref.value = "";
     linkTarget.value = "";
   }
 
+  /**
+   * Applies the href/target values to the current link in the editor.
+   */
   function linkSave() {
     editor
       .chain()
@@ -29,6 +53,9 @@ export function useLink(editor: Editor) {
     linkClose();
   }
 
+  /**
+   * Removes the link from the current selection.
+   */
   function linkRemove() {
     editor.chain().focus().extendMarkRange("link").unsetLink().run();
   }
